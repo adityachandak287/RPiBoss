5ca21662a382ea17a1faeb8e9d3ffbf6d4d8be48
Aditya Chandak <adityachandak287@gmail.com>
2019-09-14 01:03:16 +0530

    Updated RPMS functionality

--- RPMS/flaskserver.py
+++ RPMS/flaskserver.py
@@ -2,13 +2,16 @@ from flask import Flask, render_template
 from flask_socketio import SocketIO, send, emit
 import random
 from time import sleep, localtime, strftime
-from generateData import getData
+from generateData import generateDevices, generateLogs
 from flask_cors import CORS, cross_origin
-
+import threading
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
 socketio = SocketIO(app, cors_allowed_origins="*")
+
+devices = []
+logs = []
 
 
 @app.route('/')
@@ -16,29 +19,35 @@ def do_get():
     return render_template('./testClient.html')
 
 
-@socketio.on('newConnection')
-def handle_message(message):
-    print(message)
-    print("New Connection")
-    ct = strftime("%H:%M:%S", localtime())
-    # data = {"devices": [{"ip": "10.10.10.10",
-    #                      "time": ct,
-    #                      "status": "active"}],
-    #         "logs": [{"ip": "10.10.10.10",
-    #                   "message": "Device healthy"}]}
-    # sleep(1)
-    emit('gotData', getData())  # , broadcast=True)
+@socketio.on('rpmsDevices')
+def devicesFromRpms(rpmsDevices):
+    devices = rpmsDevices
+    emit('updateDevices', devices, broadcast=True)
 
 
-'''
-TODO:
+@socketio.on('rpmsLogs')
+def logsFromRpms(rpmsLogs):
+    logs = rpmsLogs
+    emit('updateLogs', logs, broadcast=True)
+
+
 @socketio.on('getDevices')
 def sendDevices(message):
+    # devices = generateDevices()
+    emit('updateDevices', devices)
+
 
 @socketio.on('getLogs')
 def sendLogs(message):
-'''
+    # logs = generateLogs()
+    emit('updateLogs', logs)
+
+
+def startSocketServer():
+    socketio.run(app, host='192.168.43.66', port=8080, debug=True)
 
 
 if __name__ == '__main__':
-    socketio.run(app, host='192.168.43.66', port=8080, debug=True)
+    devices = generateDevices()
+    logs = generateLogs()
+    startSocketServer()
--- RPMS/generateData.py
+++ RPMS/generateData.py
@@ -1,17 +1,10 @@
 from time import localtime, strftime
 import random
+from datetime import datetime
 
 
-def getData():
+def generateDevices():
     devices = []
-    logs = []
-    ct = strftime("%H:%M:%S", localtime())
-    data = {"devices": [{"ip": "10.10.10.10",
-                         "time": ct,
-                         "status": "active"}],
-            "logs": [{"ip": "10.10.10.10",
-                      "message": "Device Connected"}]}
-
     for i in range(10):
         ip = [str(random.randint(10, 99))]*4
         ip = ".".join(ip)
@@ -19,18 +12,26 @@ def getData():
 
         devices.append({
             "ip": ip,
-            "time": ct,
+            "timestamp": datetime.timestamp(datetime.now()),
             "status": status
         })
+    return {"devices": devices}
+
+
+def generateLogs():
+    logs = []
+    ct = strftime("%H:%M:%S", localtime())
+    for i in range(10):
+        ip = [str(random.randint(10, 99))]*4
+        ip = ".".join(ip)
+        status = random.choice(["active", "failed"])
         if random.randint(1, 10) < 4:
             logs.append({
                 "ip": ip,
-                "message": "Device Failed"
+                "timestamp": datetime.timestamp(datetime.now()),
+                "message": "Status change " + str(random.randint(10, 99))
+
             })
-    return {
-        "devices": devices,
-        "logs": logs
-    }
+    return {"logs": logs}
 
-
-print(getData())
+# print(getData())
